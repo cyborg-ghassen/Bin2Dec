5cf34f2a9f961a84fa4a83bde9bd4d9fe95d73f6
Ghassen bougacha <ghassen.bougacha@etudiant-ipeiem.utm.tn>
2021-12-19 14:08:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    window = tk.Tk()
+    window.geometry("1000x700")
+    window.title("Binary to Decimal")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
