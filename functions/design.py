c235127b9924f29d9108d76b2ebf3e098228f6fd
Ghassen bougacha <ghassen.bougacha@etudiant-ipeiem.utm.tn>
2021-12-20 11:03:49 +0100

    functions

--- functions/design.py
+++ functions/design.py
@@ -3,6 +3,17 @@ from tkinter.font import Font
 
 def label(**kwargs):
     var = StringVar()
-    label = Label(kwargs['root'], textvariable = var)
+    font = Font(size=kwargs['size'])
+    label = Label(kwargs['root'], textvariable = var, font=font)
+    label.place(relx=kwargs['pos_x'], rely=kwargs['pos_y'])
     var.set(kwargs['text'])
-    label.pack()
+
+def entry(**kwargs):
+    var = StringVar()
+    entry = Entry(kwargs['root'], textvariable = var)
+    entry.place(relx=kwargs['pos_x'], rely=kwargs['pos_y'])
+    return var
+
+def button(**kwargs):
+    button = Button(kwargs['root'], text=kwargs['text'], command=kwargs['event'])
+    button.place(relx=kwargs['pos_x'], rely=kwargs['pos_y'])
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from tkinter import *
 from functions.design import *
-from functions.functions import *
 
-def main():
-    window = Tk()
-    window.geometry("1000x700")
-    window.title("Binary to Decimal")
-    label(root=window, text="Binary to Decimal converter")
-    window.mainloop()
+def bin2dec(x):
+    return int(x, 2)
 
-if __name__ == "__main__":
-    main()
+def event(*args):
+    label(root=window, text=bin2dec(var.get()), size=14, pos_x=0.3, pos_y=0.4)
+    
+
+window = Tk()
+window.geometry("900x700")
+window.title("Binary to Decimal")
+label(root=window, text="Binary to Decimal converter", size=18, pos_x=0.35, pos_y=0.1)
+label(root=window, text="Enter a binary number: ", size=14, pos_x=0.1, pos_y=0.3)
+label(root=window, text="Decimal number is: ", size=14, pos_x=0.1, pos_y=0.4)
+var = StringVar()
+entry = Entry(window, width=35, textvariable=var)
+entry.place(relx=0.4, rely=0.31)
+button(root=window, text="Convert", pos_x=0.4, pos_y=0.5, event=event)
+window.mainloop()
